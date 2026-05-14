397514a7a68cfe84c1335f3a45ca8430d975c399
Gideon ter Maat <gideon@ezaccess.nl>
2026-05-14 16:40:42 +0200

    Switch to EZAccess.SourceGenerator and update build outputs
    Switched analyzer reference in IJmondWebApps.App.Net9.csproj to EZAccess.SourceGenerator.csproj. Added new build outputs: EZAccess.SourceGenerator.dll, .pdb, and .deps.json. Updated AssemblyInfo version and refreshed NuGet/MSBuild cache files to reflect the new source generator and dependencies.

--- obj/Debug/netstandard2.0/EZAccess.SourceGenerator.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/EZAccess.SourceGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c77ea334ff3f48dcf1382355b284c75b949f20f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5af68c2464735d0019b5fd50948b3bf1cc927ecc")]
 [assembly: System.Reflection.AssemblyProductAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
