1379faa5d423cc0493960f510e92b6af11dc3ff0
Gideon ter Maat <gideon@ezaccess.nl>
2026-05-14 17:06:18 +0200

    Remove EZAccess.SourceGenerator.Abstractions project
    Deleted the EZAccess.SourceGenerator.Abstractions project from the solution, including all source, project, and build files. Updated the solution file to remove project references and cleaned up related configuration. Also updated the AssemblyInformationalVersion in EZAccess.SourceGenerator.

--- obj/Debug/netstandard2.0/EZAccess.SourceGenerator.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/EZAccess.SourceGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06afa59f2c9767568d84c4a619ab213e5b30940b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44830e705bea3678316c5b3beb79b41e1e2b301b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
