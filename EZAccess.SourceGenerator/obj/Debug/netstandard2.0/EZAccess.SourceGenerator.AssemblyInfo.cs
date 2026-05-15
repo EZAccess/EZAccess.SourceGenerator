2241159ba4b50bf49c18eed9e7f5b4b5710c50be
Gideon ter Maat <gideon@ezaccess.nl>
2026-05-15 10:44:00 +0200

    Update to .NET SDK 10.0.300 and refresh build assets
    Upgraded SdkAnalysisLevel and runtimeIdentifierGraphPath to .NET SDK 10.0.300. Updated AssemblyInformationalVersionAttribute for both projects. Refreshed project.assets.json, cache files, and binary asset caches to reflect new SDK and dependency state.

--- EZAccess.SourceGenerator/obj/Debug/netstandard2.0/EZAccess.SourceGenerator.AssemblyInfo.cs
+++ EZAccess.SourceGenerator/obj/Debug/netstandard2.0/EZAccess.SourceGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be36bf9ad91ff4b5cc964113c08565cc33981a7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f3c6ec9728f4133a6315b28ac4d17d0cb6840fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EZAccess.SourceGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
